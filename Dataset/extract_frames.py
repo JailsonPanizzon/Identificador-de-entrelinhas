09c30ca0df5024015ea394d156e88e8ae858f426
Jailson Panizzon <jailson.panizzon@gmail.com>
2020-12-01 16:00:56 -0300

    Scripts de extração e imagens da manhã

---
+++ Dataset/extract_frames.py
@@ -0,0 +1,57 @@
+import cv2
+import os
+import glob
+folder = "manha"
+VIDEO_PATHS =  os.path.abspath(os.path.join('.', "Videos/"+folder))
+
+VIDEO_PATHS_all = glob.glob(os.path.join(VIDEO_PATHS, "*.*"))
+print(VIDEO_PATHS)
+print(VIDEO_PATHS_all)
+#"manha"
+cont = 0
+#"tarde1"
+#"tarde2"
+#"tarde3" 
+
+def extract_images():
+    local_cont = cont
+    cont2 = 0
+    for path in VIDEO_PATHS_all:
+
+        print("\n\n ======= Extracting Video in "+ path +"  \n\n ======")
+        cap = cv2.VideoCapture(path)
+        cap.open(path)
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        while length > 2:
+            ret, frame = cap.read()
+            length -= 1
+            #Esse if serve pra quando houve problemas nos frames
+            if ret:
+                resize_and_save(frame,local_cont)
+                #cv2.imshow("shown",frame)
+                cont2 += 1
+                if cont2 > 120:    
+                    resize_and_save(frame,local_cont)
+                    local_cont += 1
+                    print(local_cont)
+                    cont2 = 0
+            k=cv2.waitKey(30) & 0xff
+            if k == 27:
+                break
+
+def resize_and_save(frame, num):
+    #print(frame.shape[0], frame.shape[1])
+    width = 480
+    height = int(frame.shape[0] * (width / frame.shape[1] ))
+    dim = (width, height)
+    img = cv2.resize(frame, dim)
+
+    img_path = os.path.abspath(os.path.join('.', "Images/"+folder+"/"+str(num)+".png"))
+    #print(img_path)
+    #cv2.imshow("shown2",img)
+
+    cv2.imwrite(img_path, img)
+
+
+if __name__ == '__main__':  
+  extract_images()
